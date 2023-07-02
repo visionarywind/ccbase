61e5d9d03c2cd004b5bd7187c7592779a72dbd72
shanfeng <jiangshanfeng@huawei.com>
2023-07-02 22:15:08 +0800

    [feature] import gtest
    Signed-off-by: shanfeng <jiangshanfeng@huawei.com>

---
+++ gtests/src/count_down_latch_test.cpp
@@ -0,0 +1,15 @@
+//
+// Created by jiangshanfeng on 2023/7/2.
+//
+#include <gtest/gtest.h>
+#include <iostream>
+#include "tools/tool.h"
+
+using namespace std;
+
+TEST(HelloTest, TestCountDownLatch) {
+    cout << "TestCountDownLatch unit test." << endl;
+    auto count_down_latch_ptr = std::make_shared<CountDownLatch>(1);
+    count_down_latch_ptr->count_down();
+    EXPECT_EQ(0, count_down_latch_ptr->count());
+}
---
+++ gtests/src/count_down_latch_test.h
@@ -0,0 +1,11 @@
+//
+// Created by jiangshanfeng on 2023/7/2.
+//
+
+#ifndef CCBASE_HELLO_TEST_H
+#define CCBASE_HELLO_TEST_H
+
+class HelloTest {
+};
+
+#endif //CCBASE_HELLO_TEST_H
