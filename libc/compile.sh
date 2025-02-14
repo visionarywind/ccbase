a07578ed271154792a076a5155b3ff8b878f4762
shanfeng <jiangshanfeng@huawei.com>
2025-02-14 11:23:13 +0800

    add jemalloc

--- libc/compile.sh
+++ libc/compile.sh
@@ -1,4 +1,4 @@
 clear
 # g++ -shared -fPIC libc_stat.cc init.cc ms_print.cc -o libmy_mem.so -ldl # -lunwind
-g++ -shared -fPIC lib.cc concurrent_queue.cc init.cc ms_print.cc -o libmy_mem.so -ldl -lunwind -lpthread
+g++ -shared -fPIC lib.cc concurrent_queue.cc init.cc ms_print.cc -o libmy_mem.so -ldl -lunwind -lpthread -ljemalloc
 clang++ -std=c++17 test.cc -L. -lmy_mem
--- libc/lib.h
+++ libc/lib.h
@@ -4,8 +4,14 @@
 #define EXPORT extern "C" __attribute__((visibility("default")))
 #define ALIAS(name) __attribute__((alias(#name)))
 
-EXPORT void *malloc(size_t size);
-// EXPORT void *calloc(size_t nmemb, size_t size);
-// EXPORT void *realloc(void *ptr, size_t size);
-// EXPORT void *aligned_alloc(size_t alignment, size_t size);
-EXPORT void free(void *ptr);
+#if defined(__cplusplus)
+#  define CXX_THROW noexcept (true)
+#else
+#  define CXX_THROW
+#endif
+
+EXPORT void *malloc(size_t size) CXX_THROW;
+EXPORT void *calloc(size_t nmemb, size_t size) CXX_THROW;
+EXPORT void *realloc(void *ptr, size_t size) CXX_THROW;
+EXPORT void *aligned_alloc(size_t alignment, size_t size) CXX_THROW;
+EXPORT void free(void *ptr) CXX_THROW;
