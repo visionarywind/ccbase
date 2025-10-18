c8e34288d44ff209cb5d905d65a854ab9f38fb31
shanfeng <jiangshanfeng@huawei.com>
2025-10-18 16:26:32 +0800

    Fix state compress and add llm book

--- algorithm/dp/583.cc
+++ algorithm/dp/583.cc
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+using namespace std;
 /**
  * 给定两个单词 s1 和 s2 ，返回使得 s1 和 s2 相同所需的最小步数。每步可以删除任意一个字符串中的一个字符。
  * 函数签名如下：
@@ -7,60 +8,62 @@
  * 比如输入 s1 = "sea" s2 = "eat"，算法返回 2，第一步将 "sea" 变为 "ea" ，第二步将 "eat" 变为 "ea"。
  */
 
-size_t min_distance(const std::string &left, const std::string &right) {
-  auto llen = left.length();
-  auto rlen = right.length();
+size_t min_distance(const std::string &word1, const std::string &word2) {
+  auto len1 = word1.length();
+  auto len2 = word2.length();
 
-  if (llen == 0) {
-    return rlen;
-  } else if (rlen == 0) {
-    return llen;
+  if (len1 == 0) {
+    return len2;
+  } else if (len2 == 0) {
+    return len1;
   }
 
-  std::vector<std::vector<size_t>> dp(llen + 1, std::vector<size_t>(rlen + 1, 0));
-  for (size_t i = 0; i < llen; i++) {
+  vector<vector<size_t>> dp(len1 + 1, vector<size_t>(len2 + 1, 0));
+  for (size_t i = 0; i <= len1; i++) {
     dp[i][0] = i;
   }
-  for (size_t j = 0; j < rlen; j++) {
+  for (size_t j = 0; j <= len2; j++) {
     dp[0][j] = j;
   }
-  for (size_t i = 0; i < llen; i++) {
-    for (size_t j = 0; j < rlen; j++) {
-      if (left[i] == right[j]) {
+  for (size_t i = 0; i < len1; i++) {
+    for (size_t j = 0; j < len2; j++) {
+      if (word1[i] == word2[j]) {
         dp[i + 1][j + 1] = dp[i][j];
       } else {
-        dp[i + 1][j + 1] = std::min(dp[i + 1, j], dp[i, j + 1]) - 1;
+        dp[i + 1][j + 1] = std::min(dp[i + 1][j], dp[i][j + 1]) + 1;
       }
     }
   }
-  return dp[llen][rlen];
+  return dp[len1][len2];
 }
 
-size_t min_distance_sc(const std::string &left, const std::string &right) {
-  auto llen = left.length();
-  auto rlen = right.length();
+size_t min_distance_sc(const std::string &word1, const std::string &word2) {
+  auto len1 = word1.length();
+  auto len2 = word2.length();
 
-  if (llen == 0) {
-    return rlen;
-  } else if (rlen == 0) {
-    return llen;
+  if (len1 == 0) {
+    return len2;
+  } else if (len2 == 0) {
+    return len1;
   }
 
-  std::vector<size_t> dp(rlen + 1, 0);
-  for (size_t j = 0; j < rlen; j++) {
-    dp[j] = j;
+  vector<size_t> dp(len2 + 1, 0);
+  for (size_t i = 0; i <= len2; i++) {
+    dp[i] = i;
   }
-  for (size_t i = 0; i < llen; i++) {
-    size_t prev = 0;
-    for (size_t j = 0; j < rlen; j++) {
+
+  for (size_t i = 0; i < len1; i++) {
+    size_t prev = dp[0];
+    dp[0] = i + 1;
+    for (size_t j = 0; j < len2; j++) {
       size_t temp = dp[j + 1];
-      if (left[i] == right[j]) {
+      if (word1[i] == word2[j]) {
         dp[j + 1] = prev;
       } else {
-        dp[j + 1] = std::min(dp[j], dp[j + 1]) + 1;
+        dp[j + 1] = std::min({prev, dp[j], dp[j + 1]}) + 1;
       }
       prev = temp;
     }
   }
-  return dp[rlen];
+  return dp[len2];
 }
---
+++ algorithm/greed/3397.cc
@@ -0,0 +1,58 @@
+/**
+ * 给你一个整数数组 nums 和一个整数 k。
+
+    你可以对数组中的每个元素 最多 执行 一次 以下操作：
+
+    将一个在范围 [-k, k] 内的整数加到该元素上。
+    返回执行这些操作后，nums 中可能拥有的不同元素的 最大 数量。
+
+    
+
+    示例 1：
+
+    输入： nums = [1,2,2,3,3,4], k = 2
+
+    输出： 6
+
+    解释：
+
+    对前四个元素执行操作，nums 变为 [-1, 0, 1, 2, 3, 4]，可以获得 6 个不同的元素。
+
+    示例 2：
+
+    输入： nums = [4,4,4,4], k = 1
+
+    输出： 3
+
+    解释：
+
+    对 nums[0] 加 -1，以及对 nums[1] 加 1，nums 变为 [3, 5, 4, 4]，可以获得 3 个不同的元素。
+
+    
+
+    提示：
+
+    1 <= nums.length <= 105
+    1 <= nums[i] <= 109
+    0 <= k <= 109
+*/
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        std::sort(nums.begin(), nums.end());
+        int prev = INT_MIN;
+        int ans = 0;
+        for (int num : nums) {
+            int minVal = std::min(std::max(prev + 1, num - k), num + k);
+            if (prev < minVal) {
+                prev = minVal;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
