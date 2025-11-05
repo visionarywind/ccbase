141b88af26ef3b92de7ff27afd1028a29bd8c70d
shanfeng <jiangshanfeng@huawei.com>
2025-11-05 17:49:00 +0800

    add leetcode 331, 437, 494

---
+++ algorithm/tree/331.cc
@@ -0,0 +1,61 @@
+/**
+ *
+序列化二叉树的一种方法是使用 前序遍历
+。当我们遇到一个非空节点时，我们可以记录下这个节点的值。如果它是一个空节点，我们可以使用一个标记值记录，例如 #。
+
+
+例如，上面的二叉树可以被序列化为字符串 "9,3,4,#,#,1,#,#,2,#,6,#,#"，其中 # 代表一个空节点。
+
+给定一串以逗号分隔的序列，验证它是否是正确的二叉树的前序序列化。编写一个在不重构树的条件下的可行算法。
+
+保证 每个以逗号分隔的字符或为一个整数或为一个表示 null 指针的 '#' 。
+
+你可以认为输入格式总是有效的
+
+例如它永远不会包含两个连续的逗号，比如 "1,,3" 。
+注意：不允许重建树。
+
+示例 1:
+输入: preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#"
+输出: true
+示例 2:
+输入: preorder = "1,#"
+输出: false
+示例 3:
+输入: preorder = "9,#,#,1"
+输出: false
+
+提示:
+1 <= preorder.length <= 104
+preorder 由以逗号 “，” 分隔的 [0,100] 范围内的整数和 “#” 组成
+ *
+ */
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  bool isValidSerialization(string preorder) {
+    char *stk = new char[preorder.length()];
+    int pos = -1;
+    for (int i = 0; i < preorder.length(); i++) {
+      if (preorder[i] == ',') {
+        continue;
+      }
+      if (preorder[i] == '#') {
+        while (pos >= 1 && stk[pos - 1] != '#' && stk[pos] == '#') {
+          pos -= 2;
+        }
+        stk[++pos] = '#';
+      } else {
+        while (preorder[i] != ',' && i < preorder.length()) {
+          i++;
+        }
+        stk[++pos] = 'N';
+      }
+    }
+    bool ans = (pos == 0 && stk[pos] == '#');
+    delete[] stk;
+    return ans;
+  }
+};
